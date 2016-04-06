232b1481baa27fc7be17d2119f728dc37f827812
Raidel Hernandez <raidel123@hotmail.com>
2016-04-06 13:17:01 -0400

    login Page edited

--- app/src/main/java/uchat/uchat/CreateUser.java
+++ app/src/main/java/uchat/uchat/CreateUser.java
@@ -10,4 +10,6 @@ public class CreateUser extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_user);
     }
+
+
 }
--- app/src/main/java/uchat/uchat/LoginActivity.java
+++ app/src/main/java/uchat/uchat/LoginActivity.java
@@ -1,13 +1,27 @@
 package uchat.uchat;
 
+import android.content.res.Resources;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    ImageView login_logo;
+    EditText login_username, login_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        login_logo = (ImageView) findViewById(R.id.logo_login);
+        login_username = (EditText) findViewById(R.id.username);
+        login_password = (EditText) findViewById(R.id.password);
+
+        login_logo.setImageResource(R.mipmap.uchat_logo);
+
     }
 }
