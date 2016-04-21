52937e8361edc3638d494632919dd273c002c3b1
NickFeraud <nickferaud@gmail.com>
2016-04-21 19:43:25 -0400

    Completed rate my professor

--- app/src/main/java/uchat/uchat/BrowseRatings.java
+++ app/src/main/java/uchat/uchat/BrowseRatings.java
@@ -3,13 +3,35 @@ package uchat.uchat;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class BrowseRatings extends AppCompatActivity {
 
     ListView review;
+
+    String name;
+    String url="http://73.42.47.33/rmp-browse.php";
+    StringRequest request;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,22 +40,51 @@ public class BrowseRatings extends AppCompatActivity {
 
         review = (ListView) findViewById(R.id.reviews);
 
-        String []courses = getResources().getStringArray(R.array.courses);
-        Reviews []reviews = new Reviews[courses.length-1];
-        float rating = (float)3.2;
-        int j = 0;
-        for(int i = 1; i < courses.length; i++)
-        {
-            String rating_str = Float.toString(rating) + "/5.0";
-            String descrip = "The teacher was amazing.\nThis is the next line here.";
-            reviews[j] = new Reviews(courses[i], rating_str, descrip);
-            j++;
+        Bundle extras = getIntent().getExtras();
+        if (extras != null){
+            name = extras.getString("name");
         }
 
-        ReviewAdapter adapter = new ReviewAdapter(this,
-                R.layout.single_view_review, reviews);
+        Log.i("NAME:::::", name);
 
-        // Assign adapter to ListView
-        review.setAdapter(adapter);
+        request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>(){
+            @Override
+            public void onResponse(String response){
+                try{
+                    JSONArray ja = new JSONArray(response);
+                    Reviews []reviews = new Reviews[ja.length()];
+                    for(int i = 0; i < ja.length(); i++){
+                        JSONObject jo = ja.getJSONObject(i);
+                        Log.i("IN RESPONSE:::::", jo.getString("name"));
+                        reviews[i] = new Reviews( jo.getString("name"),jo.getString("rating"),jo.getString("review"));
+                    }
+                    ReviewAdapter adapter = new ReviewAdapter(BrowseRatings.this,
+                            R.layout.single_view_review, reviews);
+
+                    // Assign adapter to ListView
+                    review.setAdapter(adapter);
+
+                }catch(JSONException e){
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError error){
+                Toast.makeText(BrowseRatings.this, "Error connecting to database", Toast.LENGTH_LONG).show();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                HashMap<String,String> hashMap = new HashMap<String, String>();
+                hashMap.put("prof", name);
+                return hashMap;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(BrowseRatings.this);
+        requestQueue.add(request);
+
     }
 }
--- app/src/main/java/uchat/uchat/ViewProfessors.java
+++ app/src/main/java/uchat/uchat/ViewProfessors.java
@@ -7,44 +7,84 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ViewProfessors extends AppCompatActivity {
 
     ListView professors;
+    StringRequest request;
+    String url = "http://73.42.47.33/rmp-view.php";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_professors);
-
         professors = (ListView) findViewById(R.id.prof_ratings);
 
-        String []names = getResources().getStringArray(R.array.professors);
-        Professors []prof_list = new Professors[names.length-1];
-        float rating = (float)3.2;
-        int j = 0;
-        for(int i = 1; i < names.length; i++)
-        {
-            String rating_str = Float.toString(rating) + "/5.0";
-            prof_list[j] = new Professors(names[i], rating_str);
-            j++;
-        }
+        request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>(){
+            @Override
+            public void onResponse(String response){
+                try{
+                    JSONArray ja = new JSONArray(response);
+                    Professors []prof_list  =  new Professors[ja.length()];
+                    for(int i = 0; i < ja.length(); i++){
+                        JSONObject jo = ja.getJSONObject(i);
+                        prof_list[i] = new Professors( jo.getString("name"),jo.getString("rating"));
+                    }
 
-        ProfessorsAdapter adapter = new ProfessorsAdapter(this,
-                R.layout.single_view_prof, prof_list);
+                    ProfessorsAdapter adapter = new ProfessorsAdapter(ViewProfessors.this, R.layout.single_view_prof, prof_list);
 
-        // Assign adapter to ListView
-        professors.setAdapter(adapter);
+                    // Assign adapter to ListView
+                    professors.setAdapter(adapter);
+
+                }catch(JSONException e){
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError error){
+                Toast.makeText(ViewProfessors.this,"Error connecting to database",Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+        RequestQueue requestQueue = Volley.newRequestQueue(ViewProfessors.this);
+        requestQueue.add(request);
+
+
+
 
         professors.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             @Override
             public void onItemClick(AdapterView<?> arg0, View arg1,
                                     int position, long arg3) {
-
-                // get the professors reviews
-
-                startActivity(new Intent(ViewProfessors.this, BrowseRatings.class));
+                TextView tv = (TextView) arg1.findViewById(R.id.prof_name);
+                Bundle bundle = new Bundle();
+                bundle.putString("name", tv.getText().toString());
+                Intent intent = new Intent(ViewProfessors.this, BrowseRatings.class);
+                intent.putExtras(bundle);
+                startActivity(intent);
             }
         });
     }
